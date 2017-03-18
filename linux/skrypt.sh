c7b708ac2e0d3b42b478d55f417723a531a7c9d7
jacekardanowski <jacekard@gmail.com>
2017-03-18 15:43:25 +0100

    Update skrypt.sh

--- linux/skrypt.sh
+++ linux/skrypt.sh
@@ -2,4 +2,4 @@
 grep "OK DOWNLOAD" cdlinux.ftp.log | cut -d '"' -f 2,4 | sort | uniq | cut -d '"' -f 2 | sed "s#.*/##" | grep '\.iso' > plik1.txt
 
 grep " 200" cdlinux.www.log | cut -d " " -f 1,7,9 | sort | uniq | cut -d " " -f 1,2 |grep "\.iso" | sort | uniq | sed "s#.*/##" | sed "s#iso.*#iso#" >> plik1.txt
-sort plik1.txt | uniq -c
+sort plik1.txt | uniq -c > wynik.txt
