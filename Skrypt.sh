5497e8b1ed8e43065f88167cb175d3b885515fb0
jacekardanowski <jacekard@gmail.com>
2017-04-09 00:02:30 +0200

    update Skrypt.sh

--- Skrypt.sh
+++ Skrypt.sh
@@ -2,7 +2,7 @@
 MSG1=`dialog --stdout --title "Witaj!" --backtitle "Duzy skrypt - Wyszukiwarka plików .mp3 po tagach id3 -- Program do ustawiania tagów id3 ..." --msgbox "Wcisnij ENTER aby kontynuowac" 8 40 `
 echo $MSG1
 
-MENU=`dialog --stdout --title "Wyszukiwarka" --menu "Menu" 10 60 5 "1" "Ustaw tagi" "2" "Wyszukaj po tagach" "3" "Wyjdz"`
+MENU=`dialog --stdout --title "Wyszukiwarka" --menu "Menu" 10 60 5 "1" "Ustaw wybrane metadane" "2" "Wyszukaj po metadanych" "3" "Wyjdz"`
 
 if [ $MENU -eq 1 ] ; then
 
@@ -15,29 +15,32 @@ dialog --title "Blad" --infobox "Nie ma takiego pliku!" 8 40
 else 
 chmod 777 $WYNIK
 
-MENU2=`dialog --stdout --title "Ustawianie informacji o pliku:" --menu "Menu" 15 60 5 "1" "Ustaw artyste" "2" "Ustaw nazwe albumu" "3" "Ustaw nazwe piosenki" "4" "Ustaw rok" "5" "Ustaw numer sciezki"`
+MENU2=`dialog --stdout --title "Ustawianie informacji o pliku:" --menu "Menu" 15 60 7 "1" "Ustaw artyste" "2" "Ustaw nazwe albumu" "3" "Ustaw nazwe piosenki" "4" "Ustaw rok" "5" "Ustaw numer sciezki" "6" "Wyjdz"`
 
 case "$MENU2" in
 "1") OPCJA=`dialog --stdout --title "Ustawianie tagów" --inputbox "Podaj nazwe artysty" 8 60 ` 
-id3v2 -a $OPCJA $SCIEZKA;;
+id3v2 -a "$OPCJA" $SCIEZKA;;
 "2") OPCJA=`dialog --stdout --title "Ustawianie tagów" --inputbox "Podaj nazwe albumu" 8 60 ` 
-id3v2 -A $OPCJA $SCIEZKA;;
+id3v2 -A "$OPCJA" $SCIEZKA;;
 "3") OPCJA=`dialog --stdout --title "Ustawianie tagów" --inputbox "Podaj nazwe piosenki" 8 60 `
-id3v2 -t $OPCJA $SCIEZKA;;
+id3v2 -t "$OPCJA" $SCIEZKA;;
 "4") OPCJA=`dialog --stdout --title "Ustawianie tagów" --inputbox "Podaj rok" 8 60 `
-id3v2 -y $OPCJA $SCIEZKA;;
+id3v2 -y "$OPCJA" $SCIEZKA;;
 "5") OPCJA=`dialog --stdout --title "Ustawianie tagów" --inputbox "Podaj numer sciezki" 8 60 `
 id3v2 -T $OPCJA $SCIEZKA;;
+"6") dialog --stdout --title "Witaj!" --backtitle " " --msgbox "Do widzenia!" 8 40 exit 1;
 esac
 
-dialog --clear
-echo "Super!"
-id3v2 -C $SCIEZKA
+WARTOSC=`dialog --stdout --title "Sukces" --backtitle " "  --yesno "Pomyslnie zaktualizowano metadane\nCzy chcesz kontynuowac?" 8 60 `
+
+echo $?
+echo $WARTOSC
+
 fi
 
 elif [ $MENU -eq 2 ] ; then
 
-MENU3=`dialog --stdout --title "Ustawianie informacji o pliku:" --menu "Menu" 15 60 5 "1" "Podaj artyste" "2" "Podaj nazwe albumu" "3" "Podaj nazwe piosenki" "4" "Podaj rok" "5" "Podaj numer sciezki"`
+MENU3=`dialog --stdout --title "Wyszukiwanie po tagach:" --menu "Menu" 20 60 5 "1" "Podaj artyste" "2" "Podaj nazwe albumu" "3" "Podaj nazwe piosenki" "4" "Podaj rok" "5" "Podaj numer sciezki" "6" "Wyjdz"`
 
 case "$MENU3" in
 "1") OPCJA=`dialog --stdout --title "Wyszukiwanie po tagach" --inputbox "Szukaj po artyscie" 8 60 `;;
@@ -45,25 +48,22 @@ case "$MENU3" in
 "3") OPCJA=`dialog --stdout --title "Wyszukiwanie po tagach" --inputbox "Szukaj po nazwie piosenki" 8 60 `;;
 "4") OPCJA=`dialog --stdout --title "Wyszukiwanie po tagach" --inputbox "Szukaj po roku wydania" 8 60 `;;
 "5") OPCJA=`dialog --stdout --title "Wyszukiwanie po tagach" --inputbox "Szukaj po numerze sciezki" 8 60 `;;
+"6") exit 1;
 esac
 
-find /home/ -name "*.mp3" 2>/dev/null > tmp.txt
-chmod 777 tmp.txt
-touch tmp2.txt
-chmod 777 tmp2.txt
-while read PATH; do
-`id3v2 -R $PATH >> tmp2.txt`
-done <tmp.txt
+find /home/ -name "*.mp3" -exec id3info {} \; >> tmp.txt
 
-WYNIK=`grep $OPCJA tmp2.txt`
-if [ -n $WYNIK ] ; then
-dialog --title "Blad" --infobox "Znaleziono!" 8 40
+WYNIK=`grep "$OPCJA" tmp.txt | wc -l`
+
+if [ $WYNIK -gt 0 ] ; then
+WYNIK=`grep "$OPCJA" tmp.txt`
+dialog --stdout --title "Sukces" --msgbox "Znaleziono! $WYNIK" 8 40
+read
+dialog --stdout --title "Witaj!" --backtitle " " --msgbox "Do widzenia!" 8 40
 else
-dialog --title "Blad" --infobox "Nie znaleziono!" 8 40
+dialog --stdout --title "Porazka" --msgbox "Nie znaleziono podanej informacji:\n$OPCJA!" 8 40
 fi
 
-#id3v2 -C $SCIEZKA
-
-else echo "Koniec programu!"
+else dialog --stdout --title "Witaj!" --backtitle " " --msgbox "Do widzenia!" 8 40
 
 fi
