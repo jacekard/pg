63202825a7d0ced506850fc7a2fae7c221ed6b61
jacekardanowski <jacekard@gmail.com>
2017-04-11 14:08:22 +0200

    Update getopts.sh

--- getopts.sh
+++ getopts.sh
@@ -11,6 +11,9 @@
 # Licensed under GPL (see /usr/share/common-licenses/GPL for more details
 # or contact # the Free Software Foundation for a copy)
 
+#DOŁĄCZANIE ZEWNETRZNEJ 'Biblioteki' zmiennych, funkcji, do których można się odwołać
+. skrypt.rc
+####
 
 while getopts hvf:q  OPT; do
   case $OPT in
